042e3dd505abe740f903ff12bab19c27abbef748
gabrielWilfried <gabrielwilfried0808@gmail.com>
2023-10-31 15:51:03 +0100

    Executing the code and addition of panorama.jpg

--- panorama.py
+++ panorama.py
@@ -1,57 +1,73 @@
 import cv2
 import numpy as np
-import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 
 try:
-     # Step 1: Load the images to be stitched together.
-    images = []
-    num_images = int(input("Enter the number of images to stitch: "))
+    # Step 1: Load the images to be stitched together
+    num_images = 0
+   
+
+    while num_images <= 0:
+        try:
+            num_images = int(input("Enter the number of images to stitch: "))
+            if num_images <= 0:
+                print("Please enter a positive integer.")
+        except ValueError:
+            print("Please enter a valid integer value.")
 
     # Open a file dialog to select the images
-    Tk().withdraw()  
-    filenames = askopenfilenames(title="Select Images", filetypes=(("Image files", "*.jpg;*.jpeg;*.png"), ("All files", "*.*")))
-    filenames = list(filenames)[:num_images] 
+    Tk().withdraw()
+    filenames = askopenfilenames(
+        title="Select Images",
+        filetypes=(("Image files", "*.jpg;*.jpeg;*.png"), ("All files", "*.*")),
+    )
+    filenames = list(filenames)[:num_images]
 
+    if len(filenames) < num_images:
+        raise ValueError("Insufficient number of images selected.")
+
+    images = []
+    stitched_image = None
+    orb = cv2.ORB_create()
+
+    # Load and stitch the images
     for filename in filenames:
         image = cv2.imread(filename)
         if image is None:
-            print("Failed to load the image. Please try again.")
+            print(f"Failed to load the image '{filename}'. Please make sure it is a valid image file.")
             continue
-        images.append(image)
-    # create a feature detector
-    orb = cv2.ORB_create()
 
-    # Find keypoints and descriptors for each image
-    keypoints = []
-    descriptors = []
-    for image in images:
-        keypoint, descriptor = orb.detectAndCompute(image, None)
-        keypoints.append(keypoint)
-        descriptors.append(descriptor)
 
-    # Match feature points between the images.
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(descriptors[0], descriptors[1])
-    matches = sorted(matches, key=lambda x: x.distance)
+        if stitched_image is None:
+            stitched_image = image
+        else:
+            # Find keypoint matches between the current image and the stitched image
+            gray1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray2 = cv2.cvtColor(stitched_image, cv2.COLOR_BGR2GRAY)
 
-    # Estimate the homography matrix between each pair of images.
-    src_pts = np.float32([keypoints[0][m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-    dst_pts = np.float32([keypoints[1][m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-    homography, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            keypoints1, descriptors1 = orb.detectAndCompute(gray1, None)
+            keypoints2, descriptors2 = orb.detectAndCompute(gray2, None)
 
-    # Warp the images using the homography matrices to align them.
-    result = cv2.warpPerspective(images[0], homography, (images[0].shape[1] + images[1].shape[1], images[0].shape[0]))
-    result[0:images[1].shape[0], 0:images[1].shape[1]] = images[1]
+            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+            matches = bf.match(descriptors1, descriptors2)
+            matches = sorted(matches, key=lambda x: x.distance)
 
-    # Step 6: Blend the warped images together to create a panorama.
-    gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-    _, mask = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
-    panorama = cv2.bitwise_and(result, result, mask=mask)
+            src_pts = np.float32([keypoints1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+            dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
 
-    # Step 7: Save the panorama to a file.
-    cv2.imwrite("panorama.jpg", panorama)
+            # Estimate the homography matrix
+            homography, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+            # Warp the current image to align with the stitched image
+            warped_image = cv2.warpPerspective(image, homography, (stitched_image.shape[1] + image.shape[1], stitched_image.shape[0]))
+            warped_image[0:stitched_image.shape[0], 0:stitched_image.shape[1]] = stitched_image
+
+            stitched_image = warped_image
+
+    # Save the stitched panorama to a file
+    cv2.imwrite("panorama.jpg", stitched_image)
     print("Panorama successfully created and saved as 'panorama.jpg'")
+
 except Exception as e:
     print("An error occurred:", str(e))
